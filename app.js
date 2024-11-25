8fad57b6678d205d8be8313acb3701ea5162807c
Haley Visconti <hvisconti1343@outlook.com>
2024-11-25 14:42:41 +0000

    rearranged files... again

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const apiRouter = require("./routes/api-router");
+const app = express();
+//const cors = require('cors');
+
+//app.use(cors());
+
+app.use(express.json());
+app.use("/api", apiRouter);
+
+app.use((err, req, res, next) => {
+    if (err.code === '22P02') {
+        return res.status(400).send({ msg: 'Invalid Request' });
+    }
+    if (err.status) {
+        return res.status(err.status).send({ msg: err.msg })
+    }
+    next(err);
+})
+app.use((err, req, res, next) => {
+    res.status(500).send({ msg: 'Server error'})
+    console.log(err)
+})
+
+module.exports = app;
